69e172123c1b5a9d21267f92c47bf99a5ff12114
LAgrl <lizethariasc14@gmail.com>
2021-05-29 18:18:27 -0700

    hide the main container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
-// GP maps here
+// variables
+var city="";
+
+var destination = document.querySelector("#destination");
+var SearchBtn = document.querySelector("#search-btn");
+var placesContainer = document.querySelector("#places-container");
+
+
+// API key
+var APIKey = "AIzaSyD3hvG5xtniBYkSfMmW0gBSpISphcw5RUg";
+
+// function to grab the city from the text input 
+function displayPage(event){
+    event.preventDefault();
+    if(destination.val().trim()!==""){
+        city=destination.val().trim();
+        displayPlaces(city);
+    }
+}
+
+// function to hide the first page and display the places 
+function displayPlaces(city) {
+document.getElementById("main-box").style.display = "none";
+
+}
+
+
+// Event listeners
+$("#search-btn").on("click",displayPlaces);

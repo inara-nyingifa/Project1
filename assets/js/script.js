dc8ecb98e1ce424360e4af1f128fa1e0a973e2c7
LAgrl <lizethariasc14@gmail.com>
2021-05-31 22:37:20 -0700

    display map test

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,54 +8,24 @@ var mainPage = document.getElementById("main-page");
 
 
 // create a variable with the latitude and longitud
-var mylatlng = { lat: -33.866, lng: 151.196 }
-var mapOptions = {
-    center: mylatlng,
-    zoom: 15,
-    mapId: "8d193001f940fde3",
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
 };
 
-//create new containers
-function createMap() {
-    document.getElementById("main-box").style.display = "none";
-    var mapDiv = document.createElement("div");
-    mapDiv.setAttribute("id", "map");
-    mainPage.appendChild(mapDiv);
-    map = new google.maps.map(document.getElementById('map'), mapOptions)
+function displayMap() {
+document.getElementById("main-box").style.display = "none";
+var mapDiv = document.createElement("div");
+mapDiv.setAttribute("id", "map");
+mainPage.appendChild(mapDiv);
+initMap();
 }
 
  
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// get input text
-destination.addEventListener('place_changed', onPlaceChanged);
-
-function onPlaceChanged() {
-   var place = destination.getPlace(); 
-   if (!place.geometry) {
-       // user did not select a destination, reset the input field
-       document.getElementById('destination').placeholder =
-       'Where are you going?'
-   } else {
-       // Display details about the valid place
-       document.getElementById('details').innerHTML = place.name;
-   }
-}
-
-$("#search-btn").on("click", createMap);
+$("#search-btn").on("click", displayMap);
